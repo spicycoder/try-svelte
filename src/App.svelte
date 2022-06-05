e0ccf44c509d03177451c1abbe134b0d1c95d534
SpicyCoder <spk2naveen@hotmail.com>
2022-06-05 15:11:42 +0530

    reactivity + dynamic styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
   let count = 1;
-  const increment = () => {
-    count++;
-  };
+  $: square = count * count;
+  $: danger = square >= 100;
 </script>
 
 <main class="flex flex-col h-screen justify-center items-center">
-  <button on:click={increment} class="btn btn-primary w-24">{count}</button>
+  <button on:click={() => count++} class="btn btn-primary w-24 m-4">+1</button>
+  {count} square is {square}
+  <button on:click={() => count--} class="btn btn-primary w-24 m-4">-1</button>
+  <div class="alert w-auto m-4 {danger ? 'alert-error' : 'alert-success'}">
+    {danger ? 'Numbers are dangerously huge!' : 'Numbers look fine'}
+  </div>
 </main>
